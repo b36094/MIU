c52500444bba83448e1b91baef91377b6b7a33c2
Nactus Berrilli <muzycales@fabiana.(none)>
2013-03-10 15:56:43 -0700

    Bronze <--> Gold Split fix

---
+++ Bronze/js/json.js
@@ -0,0 +1,285 @@
+//	Andrei Birsan
+//	Visual Frameworks 1
+//	2/27/2013
+
+
+//Create an Object with Objects
+var json = {
+
+   "item1":{
+       "dropMenu":		["Item Type: ", "AudioCd"],
+       "nameItem":		["Name: ", "Love is Perfect"],
+       "genreItem":		["Genre/Type: ", "Disco"],
+       "lengthItem":	["Length: ", "60"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2001"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a cd."]
+   },
+   
+   "item2":{
+       "dropMenu":		["Item Type: ", "VideoDvd"],
+       "nameItem":		["Name: ", "Shoot Me Now"],
+       "genreItem":		["Genre/Type: ", "Action"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item3":{
+       "dropMenu":		["Item Type: ", "AudioTape"],
+       "nameItem":		["Name: ", "La-La Di"],
+       "genreItem":		["Genre/Type: ", "Disco"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item4":{
+       "dropMenu":		["Item Type: ", "Other"],
+       "nameItem":		["Name: ", "Car Documents"],
+       "genreItem":		["Genre/Type: ", "Action"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item5":{
+       "dropMenu":		["Item Type: ", "VideoTape"],
+       "nameItem":		["Name: ", "Fire Test"],
+       "genreItem":		["Genre/Type: ", "Adventure"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item6":{
+       "dropMenu":		["Item Type: ", "AudioCd"],
+       "nameItem":		["Name: ", "Love is Perfect"],
+       "genreItem":		["Genre/Type: ", "Disco"],
+       "lengthItem":	["Length: ", "60"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2001"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a cd."]
+   },
+   
+   "item7":{
+       "dropMenu":		["Item Type: ", "VideoDvd"],
+       "nameItem":		["Name: ", "Shoot Me Now"],
+       "genreItem":		["Genre/Type: ", "Action"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item8":{
+       "dropMenu":		["Item Type: ", "AudioTape"],
+       "nameItem":		["Name: ", "La-La Di"],
+       "genreItem":		["Genre/Type: ", "Disco"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item9":{
+       "dropMenu":		["Item Type: ", "Other"],
+       "nameItem":		["Name: ", "Car Documents"],
+       "genreItem":		["Genre/Type: ", "Action"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item10":{
+       "dropMenu":		["Item Type: ", "VideoTape"],
+       "nameItem":		["Name: ", "Fire Test"],
+       "genreItem":		["Genre/Type: ", "Adventure"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item11":{
+       "dropMenu":		["Item Type: ", "AudioCd"],
+       "nameItem":		["Name: ", "Love is Perfect"],
+       "genreItem":		["Genre/Type: ", "Disco"],
+       "lengthItem":	["Length: ", "60"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2001"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a cd."]
+   },
+   
+   "item12":{
+       "dropMenu":		["Item Type: ", "VideoDvd"],
+       "nameItem":		["Name: ", "Shoot Me Now"],
+       "genreItem":		["Genre/Type: ", "Action"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item13":{
+       "dropMenu":		["Item Type: ", "AudioTape"],
+       "nameItem":		["Name: ", "La-La Di"],
+       "genreItem":		["Genre/Type: ", "Disco"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item14":{
+       "dropMenu":		["Item Type: ", "Other"],
+       "nameItem":		["Name: ", "Car Documents"],
+       "genreItem":		["Genre/Type: ", "Action"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item15":{
+       "dropMenu":		["Item Type: ", "VideoTape"],
+       "nameItem":		["Name: ", "Fire Test"],
+       "genreItem":		["Genre/Type: ", "Adventure"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item16":{
+       "dropMenu":		["Item Type: ", "AudioCd"],
+       "nameItem":		["Name: ", "Love is Perfect"],
+       "genreItem":		["Genre/Type: ", "Disco"],
+       "lengthItem":	["Length: ", "60"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2001"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a cd."]
+   },
+   
+   "item17":{
+       "dropMenu":		["Item Type: ", "VideoDvd"],
+       "nameItem":		["Name: ", "Shoot Me Now"],
+       "genreItem":		["Genre/Type: ", "Action"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item18":{
+       "dropMenu":		["Item Type: ", "AudioTape"],
+       "nameItem":		["Name: ", "La-La Di"],
+       "genreItem":		["Genre/Type: ", "Disco"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item19":{
+       "dropMenu":		["Item Type: ", "Other"],
+       "nameItem":		["Name: ", "Car Documents"],
+       "genreItem":		["Genre/Type: ", "Action"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+
+   
+   "item20":{
+       "dropMenu":		["Item Type: ", "VideoTape"],
+       "nameItem":		["Name: ", "Fire Test"],
+       "genreItem":		["Genre/Type: ", "Adventure"],
+       "lengthItem":	["Length: ", "190"],
+       "pubDate":		["Release Date: ", "02-02-1999"],
+       "purchaseDate":	["Purchase Date: ", "11-11-2004"],
+       "favOpt":		["Is it a favorite item ?", "Yes"],
+       "bkCopy":		["Does the item have a back-up copy ?", "No"],
+       "slideRange":	["Next Revision (in months): ", "4"],
+       "notes":			["Notes: ", "This is a Dvd."]
+   },
+}
+
+
