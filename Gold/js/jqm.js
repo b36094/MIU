a6f8b425f63f830ef6172ccdb03487730725363c
Nactus Berrilli <nactus@fabiana.com>
2013-03-19 17:24:38 -0700

    saves to local storage

--- Gold/js/jqm.js
+++ Gold/js/jqm.js
@@ -1,31 +1,70 @@
-$(document).bind('pageinit', function(){
-	//Add a default value to the purchase date field
-	$('#pubDate').val(new Date());
-
-	//Capture the reset-link and assign it the form.reset
-	$('#resetP').click(function(){
-		alert("form has been reset");
-		$(‘#firstForm’)[0].reset();
-		})
-		
-
+$(document).on('pageinit', function(){
+	//code needed for home page goes here
+	// Concept implemented by the JQuery Mobile Dev. Team <--> Experimental phase. 
+	// Refactoring for this project by: Andrei B.	
+	$(function(){
+		$('[data-role="list-divider"]').toggle(function(){
+			$('.'+$(this).attr('data-link')).addClass('show');
+			$(this).children().removeClass('ui-icon-plus').addClass('ui-icon-minus');
+		},
+	
+		function(){
+			$('.'+$(this).attr('data-link')).removeClass('show');
+			$(this).children().removeClass('ui-icon-minus').addClass('ui-icon-plus');
+	 	});	
 	});
-});
-
-// Concept implemented by the JQuery Mobile Dev. Team <--> Experimental phase. 
-// Refactoring for this project by: Andrei B.
-
-$(function(){
-			$('[data-role="list-divider"]').toggle(function(){
-				$('.'+$(this).attr('data-link')).addClass('show');
-				$(this).children().removeClass('ui-icon-plus').addClass('ui-icon-minus');
-			},
+	//addItem page code goes here
+	
+	//function to parse the form
+	
+	var $myFirstForm = $('#firstForm');
+	
+	//ignores the items that have the class "ignore" on them	
+	$myFirstForm.validate({
+		ignore: ".ignore",
+		invalidHandler: function(form, validator){},
+		submitHandler: function(){
+			var data = $myFirstForm.serializeArray();
+			var randomId = genRandomId();
+			data.randomNum = randomId;			
+			var jsonObj = JSON.stringify(data);
+			localStorage.setItem(randomId,jsonObj);		
+			console.log(localStorage);
+			console.log(randomId);
+			displayData();
+			alert("Data was saved in the local storage!");
+		}
+	
+	});
+	
+});	
 		
-			function(){
-				$('.'+$(this).attr('data-link')).removeClass('show');
-				$(this).children().removeClass('ui-icon-minus').addClass('ui-icon-plus');
-    		});	
-		});
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var   displayData = function(){
+		$.post("#displayInfo", { name: "John", time: "2pm" } );
+};
+
+var genRandomId = function(){
+	var randomId = Math.floor(Math.random() * 10000001);
+	return randomId;
+};
+	
+ 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};
+
+
 
     		
 
